8bb3946e823c6dbfa4e46dfca19799711a468d8e
Maomendi <161388907+Maomendi@users.noreply.github.com>
2025-01-25 23:16:50 -0500

    Delete wd131 directory

--- wd131/week02/temple/scripts/temples.js
+++
@@ -1,8 +0,0 @@
-const ham = document.getElementById("ham");
-const navMenu = document.getElementById("nav-menu");
-
-
-ham.addEventListener("click", () => {
-    navMenu.classList.toggle("active");
-    ham.classList.toggle("is-active");
-});
